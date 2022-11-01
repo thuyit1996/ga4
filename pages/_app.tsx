90aadd8eee1e704db68c66779e33356c0ad7684a
vu thuy <vuthuycntt1996@gmail.com>
2022-11-01 13:53:31 +0700

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+
+    <Component {...pageProps} />
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-M1LCNXGQ5E"
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-M1LCNXGQ5E');
+            `}
+    </Script>
+  </>
 }
